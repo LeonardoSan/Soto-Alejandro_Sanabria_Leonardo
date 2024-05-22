3f2fdabf073ead38f49bee96d81038ed6b5b7fb4
Alem7u7 <soga981014g70@outlook.com>
2024-05-21 18:56:16 -0600

    push2

--- src/Odontologo.java
+++ src/Odontologo.java
@@ -1,4 +1,62 @@
 public class Odontologo {
 
-    /*hola*/
+    private int numMatricula;
+    private String nombre;
+    private String apellido;
+    private Integer ID;
+
+    public Odontologo(int numMatricula, String nombre, String apellido) {
+        this.numMatricula = numMatricula;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public Odontologo(int numMatricula, String nombre, String apellido, Integer ID) {
+        this.numMatricula = numMatricula;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.ID = ID;
+    }
+
+    public int getNumMatricula() {
+        return numMatricula;
+    }
+
+    public void setNumMatricula(int numMatricula) {
+        this.numMatricula = numMatricula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Integer getID() {
+        return ID;
+    }
+
+    public void setID(Integer ID) {
+        this.ID = ID;
+    }
+
+    @Override
+    public String toString() {
+        return "Odontologo{" +
+                "numMatricula=" + numMatricula +
+                ", nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", ID=" + ID +
+                '}';
+    }
 }
