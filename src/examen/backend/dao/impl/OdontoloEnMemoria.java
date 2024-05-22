893c7a0e49d64843f944fcaa0058fa6fd3047139
Leonardo Sanabria Hoyos <leonardosanh@gmail.com>
2024-05-21 20:05:53 -0500

    dao en memoria

--- src/examen/backend/dao/impl/OdontoloEnMemoria.java
+++ src/examen/backend/dao/impl/OdontoloEnMemoria.java
@@ -1,4 +1,17 @@
 package examen.backend.dao.impl;
+import java.util.ArrayList;
+import java.util.List;
 
 public class OdontoloEnMemoria {
+    private List<Odontologo> odontologos = new ArrayList<>();
+
+    @Override
+    public void guardar(Odontologo odontologo) {
+        odontologos.add(odontologo);
+    }
+
+    @Override
+    public List<Odontologo> listarTodos() {
+        return new ArrayList<>(odontologos);
+    }
 }
